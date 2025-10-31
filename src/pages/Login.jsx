4fd7e42a7c01b04d34c8e991cc4e68d1508d827a
Usama Khan <usamabin2580@gmail.com>
2025-10-31 11:36:52 -0700

    logi page padding amd margin issue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
   };
 
   return (
-    <main className="main">
+    <main className="main" style={{ paddingTop: '50px' }}>
         <section className="section-box shop-template mt-60">
           <div className="container">
             <div className="row mb-100">
